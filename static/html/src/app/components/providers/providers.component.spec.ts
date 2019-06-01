09afa30358510ae51c6359cece9c9ec418a28bee
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-06-01 15:32:49 +0000

    static/html bind/providers skeleton

--- static/html/src/app/components/providers/providers.component.spec.ts
+++ static/html/src/app/components/providers/providers.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ProvidersComponent } from './providers.component';
 
@@ -8,7 +9,8 @@ describe('ProvidersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProvidersComponent ]
+      declarations: [ ProvidersComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
