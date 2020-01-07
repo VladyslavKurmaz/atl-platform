ded1702923d167a0314cb3773b591dfd3fdb24a9
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2020-01-07 13:07:44 +0200

    Squashed 'static/web/' content from commit 7b6267f
    git-subtree-dir: static/web
    git-subtree-split: 7b6267f5463d177bd4dc12a5d23ba4bfa502a7f8

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route/*, Link, Redirect*/ } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home/Home';
+
+document.body.classList.add('container-fluid', 'p-0', 'h-100');
+
+function App() {
+  return (
+    <div className="App d-flex flex-column h-100">
+      <Navbar hostUrl="." titleShort="MVTs - React" titleLong="Minimum Viable Templates - React"/>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/home' component={Home} />
+      </Switch>
+      <Footer version="v19.12.0"/>
+    </div>
+  );
+}
+
+export default App;
